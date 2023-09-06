100a788efde9e549c2d707a28dc927a38117ee6a
Edson Junio Almeida de Bastos <edsonjabastos@hotmail.com>
2023-09-06 10:40:11 -0300

    Project initial setup

---
+++ src/test/java/io/github/com/picpaysimples/PicpaysimplesApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.com.picpaysimples;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaysimplesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
